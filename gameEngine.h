76bc09e713afdb1fd23a71cdf3841a5b6995169f
Supertitan568 <58353323+Supertitan568@users.noreply.github.com>
2021-10-14 13:48:22 -0500

    New changes
    may look weird

---
+++ gameEngine.h
@@ -0,0 +1,23 @@
+#include "SFML/Graphics.hpp"
+#include "tileMap.h"
+#include "object.h"
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
+
+class gameEngine{
+private:
+    tileMap map;
+    sf::RenderWindow window;
+    std::vector<object> allObjects;
+
+    void pollGameEvents();
+    void render();
+    void pollWindowEvents();
+    void registerKeyPress();
+public:
+    bool init();
+    void runGameLogic();
+    void runGameRender();
+    void shutdown();
+};
+#endif
